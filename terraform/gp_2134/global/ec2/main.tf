7226a615a04f805b70c2bd7bf581aef156f25e07
miles.lee <miles.lee@nsuslab.com>
2024-01-08 23:17:42 +0900

    test

--- terraform/gp_2134/global/ec2/main.tf
+++ terraform/gp_2134/global/ec2/main.tf
@@ -21,13 +21,9 @@ provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
 }
 
-resource "aws_instance" "vm_instance" {
-  ami             = "ami-05c13eab67c5d8861"                   # us-east-1 Amazon Linux 2023 AMI 2023.2.20231030.1 x86_64 HVM kernel-6.1
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.vpc_subnet.id
-  security_groups = [aws_security_group.security_group.id]
+resource "aws_vpc" "vpc_network_2" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "terraform-instance"
+    Name = "terraform-network2"
   }
 }
-
